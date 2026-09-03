d4321376ddd4637cef781c20c658879cde59712c
kidusentity14-sketch <kidus.entity14@gmail.com>
2026-09-03 14:17:51 +0000

    first

--- Javamath.java
+++ Javamath.java
@@ -1,1 +1,15 @@
+// Your name
+// This program will calculate the date easter falls on given a year after 1583.
+import java.util.*;
+public class Easter{
+    public static void main(String[] args) {
+        //code goes  k m mj      
+        Scanner scanner = new Scanner(System.in); //this allows you to read input from the user.
+        int y = scanner.nextInt(); // reads the next number the user types. 
+        int a = y % 19;
+        int b = y /100;
+        
+        System.out.println("a = "+a);
 
+    }
+}
